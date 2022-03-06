4c8ed5ad53a9120a0a78a2052f2c2302c862f027
iqbalrafzaini <91504163+iqbalrafzaini@users.noreply.github.com>
2022-03-06 11:38:11 +0700

    Add files via upload

---
+++ meterToFeetHeightConverter/main.js
@@ -0,0 +1,11 @@
+let meterbtn = document.getElementById('meter');
+let feetbtn = document.getElementById('feet');
+
+meterbtn.addEventListener('click', function () {
+  let input = document.getElementById('input').value;
+  document.getElementById('output').value = input / 3.281 + ' meter';
+});
+feetbtn.addEventListener('click', function () {
+  let input = document.getElementById('input').value;
+  document.getElementById('output').value = input * 3.281 + ' feet';
+});
